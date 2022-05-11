959b4c6eea9733c026be3e4e7127aeaf91c16efd
Mahmudjon <91727683+MahmudjonToraqulov@users.noreply.github.com>
2022-05-11 19:17:13 +0300

    nothing

--- little lotin uzbek bot/telegram_bot.py
+++ little lotin uzbek bot/telegram_bot.py
@@ -6,7 +6,6 @@ Created on Thu Jan 13 13:50:05 2022
 """
 
 
-
 from transliterate import to_cyrillic,to_latin 
 import telebot
 
@@ -15,12 +14,14 @@ TOKEN = '5091729283:AAHniWKFX97_aWz0NdNYrcv6bSgnjMj04S0'
 bot = telebot.TeleBot(TOKEN, parse_mode=None) 
 
 
+
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
     answer = "Assalomu aleykum hush kelibsiz!!! Siz o'zbek kirill botiga murojaat qildingiz!! \nSiz bu botga lotin alifbosida ma'lumot kiritsangiz kirill alifbosiga o'tkazib beradi,agar kirill alifbosida yozsangiz lotinga ozgartirib beradi!\nIltimos ma'lumot kiriting: "
     bot.reply_to(message, answer)
 
 
+    
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
     newMessage = message.text
